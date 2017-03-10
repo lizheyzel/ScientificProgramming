23ce6fa6771279b26b31bb72f43e5f78ae638bcf
Jeff Elkner <jeff@elkner.net>
2017-03-10 10:19:59 -0500

    Added shopping list example to learn files

---
+++ usingfiles/shoppinglist.py
@@ -0,0 +1,8 @@
+def load_shopping_list(fname):
+    """Load a file containing shopping list data."""
+    return ''
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile('shoppinglist.txt')
