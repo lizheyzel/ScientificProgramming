1d7f425abde6f9694c63a8f6a5d156f0ac92685d
Jeffrey Elkner <jeff@elkner.net>
2017-04-28 12:14:38 -0400

    Added comments to quadeqtools2.py

--- quadeq2/quadeqtools2.py
+++ quadeq2/quadeqtools2.py
@@ -19,12 +19,23 @@ def read_point_set_from_file(fname, line_num):
 
       >>> read_point_set_from_file('test_point_sets.dat', 1)
       [(-3, -65), (4, -16), (-10, -408)]
+      >>> read_point_set_from_file('test_point_sets.dat', 2)
+      [(-2, -40), (3, -5), (5, -33)]
     """
     f = open(fname, 'r')
+    # read in a line containing three points, stripping off the \n
+    # Example: '(-3, -65), (4, -16), (-10, -408)'
     point_str =  f.readlines()[line_num - 1][:-1]
+    # create a list of the three point strings by splitting on '), '
+    # Example: ['(-3, -65', '(4, -16', '(-10, -408)']
     points = point_str.split('), ')
+    # Remove the '(' at the beginning of each point
+    # Example: ['-3, -65', '4, -16', '-10, -408)']
     points = [point[1:] for point in points]
+    # Remove the ')' at the beginning of the last point
+    # Example: ['-3, -65', '4, -16', '-10, -408']
     points[-1] = points[-1][:-1]
+    # call str2point to turn the strings into 2-tuples with numbers
     points = [str2point(point) for point in points]
 
     return points
