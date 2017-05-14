42cdd994cd9d439fd5c4e9f71568f9c89e802591
Jeffrey Elkner <jeff@elkner.net>
2017-05-14 11:40:02 -0400

    Added polar_to_cartesian to coordinate_math

--- coordinates/coordinate_math.py
+++ coordinates/coordinate_math.py
@@ -17,6 +17,27 @@ def cartesian_to_polar(p):
     return (r, theta)
 
 
+def polar_to_cartesian(p):
+    """
+    Convert a point provided as an ordered 2-tuple representing the polar
+    coordinates to the same point in cartesian coordinates.
+
+      >>> p = polar_to_cartesian((5, 0.9272952180016122))
+      >>> int(round(p[0]))
+      3
+      >>> int(round(p[1]))
+      4
+      >>> p2 = polar_to_cartesian((13, 1.17605207095135))
+      >>> int(round(p2[0]))
+      5
+      >>> int(round(p2[1]))
+      12
+    """
+    r, theta = p[0], p[1]
+
+    return r * math.cos(theta), r * math.sin(theta)
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
